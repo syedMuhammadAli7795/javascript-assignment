d2ea12a329c2ea1c057fc15513664dda34871fa2
Syed Muhammad Ali <65005362+syedMuhammadAli7795@users.noreply.github.com>
2020-06-07 15:28:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//chapter 1
+//q1
+alert("Welcome to my website")
+//q2
+alert("Error! Please enter a valid password")
+//q3
+alert("Welcome to js land\n Happy coding")
+//q4
+alert("Welcome to js land")
+alert(" Happy coding")
+//q5
+console.log("hell0.. ican run Js through my browser web console")
+//q6
+//show in my exisiting project
+
+
+//chapter 2
+//q1
+var username;
+//q2
+var myName;
+myName ="Syed Muhammad Ali";
+//q3
+var message;
+message = "Hello World";
+alert(message);
+//q4
+var name ="Jhone Doe";
+var age ="15";
+var course ="Certified Mobile Application devlopment";
+alert(name);
+alert(age + "year old");
+alert(course)
+//q5
+var pizza ="pizza \n pizz \n piz \npi \np";
+alert(pizza)
+//q6
+var email ="example@example.com";
+alert(  "My email address is"+ email );
+//q7
+var book ="A smarter way to learn java script";
+alert(book);
+//q8
+//done in HTML file
+
+
+//q9
+var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design);
+
+
+//.........................................chapter 3..........................................................................................
+
+//q1
+var age ="15";
+alert("i am "+ age+ "yers old");
+//q2
+var num;
+num = 141;
+alert("you have visted this site"  +  num + "times");
+//q3
+//answer in html file
+//..............................................chapter 4............................................................................
+
+//q1
+var variable1 = "Hello World!", variable2 = "Testing...", variable3 = 42;
+//q2 legal
+var name ="ali";
+var group ="small";
+var animal ="cat"
+var num = 5;
+var place = "malir"
+//illegal
+//var class =4;
+//var -kat = "ali";
+//var:= 13;
+//var 22gt2 ='qww';
+//var 2= 2;
